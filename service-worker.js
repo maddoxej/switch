892a3339663a7d4bb5636705041f0612350bfe9f
maddoxej <maddoxej.users.sourceforge.net>
2020-02-11 22:11:38 -0500

    Add and remove buttons

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,13 @@
 var cachePrefix = 'switch-';
-// Change the version number in the following line each time any of the cached files changes. 
-var cacheName = cachePrefix + '-v1';
-
-var filesToCache = ['index.html','switch.js','switch.css','favicon.ico'];
-
-self.addEventListener('install', function(installEvent) {
-  console.log('[Service Worker] Install');
-  installEvent.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[Service Worker] Caching all files');
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+// Change the version number in the following line each time any of the cached files change. 
+var cacheName = cachePrefix + '-v3';
 
 self.addEventListener('activate', function(activateEvent) {
     activateEvent.waitUntil(
         caches.keys().then(function(keyList) {
             return Promise.all(keyList.map(function(key) {
                 if (key !== cacheName && key.startsWith(cachePrefix)) {
-                    console.log('[ServiceWorker] Removing old cache', key);
+                    console.log('[service-worker] Removing old cache', key);
                     return caches.delete(key);
                 }
             }));
@@ -27,11 +15,11 @@ self.addEventListener('activate', function(activateEvent) {
     )
 });
 
-
+// Override network requests by returning from cache if the network request fails. 
 self.addEventListener('fetch', function(fetchEvent) {
     fetchEvent.respondWith(
       caches.match(fetchEvent.request).then(function(cachedResponse) {
-        console.log('[Service Worker] Fetching resource: '+fetchEvent.request.url);
+        console.log('[service-worker] Fetching resource: '+fetchEvent.request.url);
         return cachedResponse || fetch(fetchEvent.request).then(function(response) {
           return caches.open(cacheName).then(function(cache) {
             console.log('[Service Worker] Caching new resource: ' + fetchEvent.request.url);
--- switch.js
+++ switch.js
@@ -1,18 +1,43 @@
 (function(){
     var localStorage = window.localStorage || {};
     localStorage.switch = localStorage.switch || "";
-    var numberOfSwitches = parseInt((new URL(document.location)).searchParams.get("s"));
-    if (!isNaN(numberOfSwitches) && numberOfSwitches > 1) {
-        for (var i = 2; i<= numberOfSwitches; i++) {
-            var template = document.getElementById("switch1");
-            var newSwitch = template.cloneNode(true);
-            newSwitch.id = "switch" + i;
-            template.parentNode.appendChild(newSwitch);
-        }
+    var numberOfSwitches = parseInt((new URL(document.location)).searchParams.get("s")) || parseInt(localStorage.switchCount);
+    if (isNaN(numberOfSwitches) || numberOfSwitches < 1 || numberOfSwitches > 99) {
+        numberOfSwitches = 1;
     }
 
-    var switches =[].slice.call(document.querySelectorAll(".switch"));
-    switches.forEach(s =>{
+    setSwitchCount(numberOfSwitches);
+
+    function setSwitchCount(numberOfSwitches) {
+        var dif = document.querySelectorAll(".switch").length - numberOfSwitches;
+        if (dif > 0) {
+            var switches =[].slice.call(document.querySelectorAll(".switch"));
+            for (var i = dif; i > 0 ; i--){
+                    var s = switches[numberOfSwitches - i + 1];
+                    s.parentNode.removeChild(s);
+                }
+        } 
+        if (dif < 0) {
+            for (var i = numberOfSwitches + dif + 1; i<= numberOfSwitches; i++) {
+                var template = document.getElementById("switch1");
+                var newSwitch = template.cloneNode(true);
+                newSwitch.id = "switch" + i;
+                var cb = newSwitch.querySelector("input");
+                cb.setAttribute("aria-label", "C" + i);
+                setSwitchValue(newSwitch);
+                template.parentNode.appendChild(newSwitch);
+            }
+        }
+
+        localStorage.switchCount = numberOfSwitches.toString();
+    }
+
+    function setAllSwitchValues() {
+        var switches =[].slice.call(document.querySelectorAll(".switch"));
+        switches.forEach(setSwitchValue);
+    }
+
+    function setSwitchValue(s){
         var cb = s.querySelector("input");
         cb.checked = localStorage.switch.includes(s.id + ",");
         cb.addEventListener('change', (event) => {
@@ -25,8 +50,9 @@
                 localStorage.switch = localStorage.switch.replace(s.id + ",","");
             } 
         });       
-        cb.disabled = false;
-    });
+    }
+
+    setAllSwitchValues();
 
     var resetButton = document.querySelector("input.reset");
     resetButton.addEventListener('click', function(){
@@ -35,7 +61,21 @@
             var cb = s.querySelector("input");
             cb.checked = false;
         });
-        
+
         localStorage.switch = "";
+    });
+
+    var removeButton = document.querySelector(".removeButton");
+    removeButton.addEventListener('click', function(){
+        var numberOfSwitches = parseInt(localStorage.switchCount) - 1;
+        if (numberOfSwitches > 0) {
+            setSwitchCount(numberOfSwitches);
+        }
+    });
+
+    var addButton = document.querySelector(".addButton");
+    addButton.addEventListener('click', function(){
+        var numberOfSwitches = parseInt(localStorage.switchCount) + 1;
+        setSwitchCount(numberOfSwitches);
     });
 })();
